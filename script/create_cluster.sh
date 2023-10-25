b1f3381bec8756c04487270d2bbe1938033cfbdd
deshabhakt <dgavali1@gmail.com>
2023-10-25 15:06:43 +0530

    create cluster

---
+++ script/create_cluster.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cluster_name=capstone
+aws_region=us-east-1
+
+if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_name} | grep -q ${cluster_name}; then
+    echo "Cluster already exists"
+else
+    echo "Creating new cluster..."
+    /.eksctl create cluster --name ${cluster_name} --region=${aws_region} --nodegroup-name cap-nodes --nodes 2
+    echo "Cluster created"
+    aws eks update-kubeconfig --region us-east-1 --name capstone
+    echo "Node Details: "
+    /.kubectl get nodes
+fi
