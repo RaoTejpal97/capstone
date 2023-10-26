31475f61eabdffc61c963f6cabe17a0cd3534976
deshabhakt <dgavali1@gmail.com>
2023-10-26 22:12:42 +0530

    create cluster

--- script/create_cluster.sh
+++ script/create_cluster.sh
@@ -3,16 +3,16 @@
 cluster_name=capstone
 aws_region=us-east-1
 
-if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_name} | grep -q ${cluster_name}; then
-    /.eksctl delete cluster --region=${aws_region} --name=${cluster_name}
-    echo "Cluster already exists"
-else
+# if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_name} | grep -q ${cluster_name}; then
+#     /.eksctl delete cluster --region=${aws_region} --name=${cluster_name}
+#     echo "Cluster already exists"
+# else
     echo "Cleanup......."
-    /.eksctl delete cluster --region=${aws_region} --name=${cluster_name}
+    /.eksctl delete cluster --region=${aws_region} --name=capstone1
     echo "Creating new cluster..."
-    /.eksctl create cluster --name ${cluster_name} --region=${aws_region} --nodegroup-name cap-nodes --nodes 2
+    /.eksctl create cluster --name capstone1 --region=${aws_region} --nodegroup-name cap-nodes --nodes 2
     echo "Cluster created"
-    aws eks update-kubeconfig --region us-east-1 --name capstone
+    aws eks update-kubeconfig --region us-east-1 --name capstone1
     echo "Node Details: "
     /.kubectl get nodes
-fi
+# fi
