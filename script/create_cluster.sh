a2b9f54a26fa33be7156301fccad998c17e984c1
deshabhakt <dgavali1@gmail.com>
2023-10-26 21:55:31 +0530

    correcting create cluster stage

--- script/create_cluster.sh
+++ script/create_cluster.sh
@@ -7,6 +7,8 @@ if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_nam
     /.eksctl delete cluster --region=${aws_region} --name=${cluster_name}
     echo "Cluster already exists"
 else
+    echo "Cleanup......."
+    /.eksctl delete cluster --region=${aws_region} --name=${cluster_name}
     echo "Creating new cluster..."
     /.eksctl create cluster --name ${cluster_name} --region=${aws_region} --nodegroup-name cap-nodes --nodes 2
     echo "Cluster created"
