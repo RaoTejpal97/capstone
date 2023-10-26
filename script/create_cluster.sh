d1d012897ca1e14bbc5b7ad8bf24c509ecfe31b3
deshabhakt <dgavali1@gmail.com>
2023-10-26 22:48:47 +0530

    create commit

--- script/create_cluster.sh
+++ script/create_cluster.sh
@@ -7,8 +7,8 @@ aws_region=us-east-1
 #     /.eksctl delete cluster --region=${aws_region} --name=${cluster_name}
 #     echo "Cluster already exists"
 # else
-    echo "Cleanup......."
-    /.eksctl delete cluster --region=${aws_region} --name=capstone1
+    # echo "Cleanup......."
+    # /.eksctl delete cluster --region=${aws_region} --name=capstone1
     echo "Creating new cluster..."
     /.eksctl create cluster --name capstone1 --region=${aws_region} --nodegroup-name cap-nodes --nodes 2
     echo "Cluster created"
